6c433ffd59b61b0b85b7971e704157aa9880ea22
Vinicius Melo <dev@sysprocard.com.br>
2024-01-29 11:42:32 -0300

    teste 3

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,7 @@
 import mysql from "mysql2/promise";
 
 export async function query(query, values) {
+  console.log(process.env.DB_PASSWORD);
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
