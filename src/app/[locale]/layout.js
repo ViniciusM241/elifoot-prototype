d48dccbc316d47105fdfa8a4fefc81290f235fa7
Vinicius Melo <dev@sysprocard.com.br>
2024-01-29 11:06:00 -0300

    Test

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -15,6 +15,8 @@ export const metadata = {
   description: "Prototipagem de reformulação de layout do jogo Elifoot",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({ children, params }) {
   const namespaces = ["common"];
   const { t } = await initTranslations(params.locale, namespaces);
